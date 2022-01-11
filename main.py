e3b23dc72a5c01e8503e33c4f1f1602534751eea
georgi kostov <georgi.kostov@cytosmart.com>
2022-01-11 12:45:12 +0100

    Adding 1st version of our code, probably not even working but still something i geuss

--- main.py
+++ main.py
@@ -1,16 +1,106 @@
-# This is a sample Python script.
+import math
+import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+P = 1
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def isPointInRect(p, rect):
+    [x, y] = p
+    [x1, x2, y1, y2] = rect
+    return x1 <= x <= x2 and y1 <= y <= y2
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def allSquaresInCircle(center, radius, squareSize):
+    nrRows = radius * 2 / squareSize
+    x1 = center[0] - radius
+    y1 = center[1] - radius
+
+    allSquares = []
+    for i in range(0, nrRows):
+        for j in range(0, nrRows):
+            allSquares.push([
+                x1 + i*squareSize, 
+                x1 + (i+1)*squareSize,
+                y1 + j*squareSize, 
+                y1 + (j+1)*squareSize
+            ])
+    return allSquares
+
+        
+
+
+def coresetConstruction(points, epsilon=0.2):
+    coreset = []
+
+    n = len(points)
+    d = 2
+    a = 1
+    logN = math.log(n)
+    z = logN * math.log(a * logN)
+
+    centers = kMeansPlusPlus()
+    radius = math.sqrt(cost(points, centers) / (a * logN * n))
+
+    for j in range(0, z):
+        radius = radius * (2**j)
+        x = epsilon * radius / math.sqrt(d)
+        
+        for center in centers:
+            for square in allSquaresInCircle(center, radius, x):
+                allPointsInSquare = []
+                for point in points:
+                    if isPointInRect(point, square):
+                        allPointsInSquare.append(point)
+                if len(allPointsInSquare) > 0:
+                    # select abritrary point as rep
+                    rep = allPointsInSquare[math.floor(random.random() * len(allPointsInSquare))]
+                    coreset.append({"point": rep, "weight": len(allPointsInSquare)})
+                    for point in allPointsInSquare:
+                        points.remove(point)
+    return coreset
+                
+
+            
+
+        
+
+
+
+
+def kMeansPlusPlus(points, k):
+    n = len(points)
+    centroids = []
+    centroids[0] = points[random.randint(0, n)]
+    minDistances = []
+    cumulative = []
+    sampledIndex = 0
+
+    for i in range(0, n):
+        minDistances[i] = math.inf # 999999999
+
+    for i in range(1, k):
+        for j in range(0, n):
+            x = distance(points[j], centroids[i-1])
+            if minDistances[j] > x:
+                minDistances[j] = x
+        cumulative[0] = minDistances[0] * minDistances[0];
+
+        for j in range (1, n):
+            cumulative[j] = cumulative[j- 1] + (minDistances[j] * minDistances[j])
+        
+        x = random.randrange(0, 1)
+        x = x*cumulative[n-1]
+
+        if x <= cumulative[0]:
+            sampledIndex = 1
+        else:
+            for j in range(1, n):
+                if x > cumulative[j-1] & x <= cumulative[j]:
+                    sampledIndex = j
+        centroids[i] = points[sampledIndex]
+
+def distance(pointA, pointB):
+    return ((((pointB[0] - pointA[0])**2) + ((pointB[1] - pointA[1])**2))**0.5)
+
+def cost(pointA, pointB):
+    return 2
